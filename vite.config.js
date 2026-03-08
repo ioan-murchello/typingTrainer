56a29064f0c276eea50d5879f827f1cbb5840c2e
Jack <jack@MacBook-Pro.local>
2026-03-08 17:12:14 +0100

    corrected vite.congig

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'  
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+
+  base: '/', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets', 
+    sourcemap: true,
+  },
 })
